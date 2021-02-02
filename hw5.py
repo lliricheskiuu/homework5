5bea5c6c3a8358814ad2da7dcd18cae168d10ebe
lliricheskiuu <kzarrr4@gmail.com>
2021-02-02 14:51:21 +0200

    10 заданий (незакоменченные задания более удовлетворяют условию)

--- hw5.py
+++ hw5.py
@@ -2,34 +2,34 @@
 
 # 1)
 
-# tmp = True
-#
-# while tmp:
-#
-#     try:
-#         value = str(int(input("Enter your value:")))
-#         print(value.count('0'))
-#         tmp = False
-#     except ValueError:
-#         print("Enter int value!")
+tmp = True
+
+while tmp:
+
+    try:
+        value = str(int(input("Enter your value:")))
+        print(value.count('0'))
+        tmp = False
+    except ValueError:
+        print("Enter int value!")
 
 # 2)
 
-# tmp = True
-#
-# while tmp:
-#     try:
-#         number = str(int(input('Enter your number:')))
-#         tmp = False
-#     except ValueError:
-#         print("Enter int value!")
-#
-# count = 0
-#
-# for value in number:
-#     if value == '0':
-#         count += 1
-# print(count)
+tmp = True
+
+while tmp:
+    try:
+        number = str(int(input('Enter your number:')))
+        tmp = False
+    except ValueError:
+        print("Enter int value!")
+
+count = 0
+
+for value in number:
+    if value == '0':
+        count += 1
+print(count)
 
 # Попробывал решить двумя способами (1 - через .count; 2 - через цикл for и проверку на '0')
 
@@ -37,16 +37,16 @@
 
 # 2. Дано целое число (int). Определить сколько нулей в конце этого числа.
 
-# number = str(int(input("Enter your number:")))
-# count = 0
-#
-# for value in number[::-1]:
-#     if value == '0':
-#         count += 1
-#     else:
-#         break
-#
-# print(count)
+number = str(int(input("Enter your number:")))
+count = 0
+
+for value in number[::-1]:
+    if value == '0':
+        count += 1
+    else:
+        break
+
+print(count)
 
 ###
 
@@ -54,19 +54,19 @@
 # Создать список my_result в который вначале поместить
 # элементы на четных местах из my_list_1, а потом все элементы на нечетных местах из my_list_2.
 
-# my_list_1 = ['Q', 'w', 'e', 'r', 't', 'y']
-# my_list_2 = ['y', 't', 'r', 'e', 'w', 'Q']
-# my_result = []
-#
-# for index in range(len(my_list_1)):
-#     if not index % 2:
-#         my_result.append(my_list_1[index])
-#
-# for index in range(len(my_list_2)):
-#     if index % 2:
-#         my_result.append(my_list_2[index])
-#
-# print(my_result)
+my_list_1 = ['Q', 'w', 'e', 'r', 't', 'y']
+my_list_2 = ['y', 't', 'r', 'e', 'w', 'Q']
+my_result = []
+
+for index in range(len(my_list_1)):
+    if not index % 2:
+        my_result.append(my_list_1[index])
+
+for index in range(len(my_list_2)):
+    if index % 2:
+        my_result.append(my_list_2[index])
+
+print(my_result)
 
 ###
 
@@ -74,27 +74,27 @@
 # вначале поместить четные элементы (ИМЕННО ЭЛЕМЕНТЫ) из my_list_1 и потом нечетные элементы из my_list_2.
 # my_list_1 = [1,3,2,4,5], my_list_2 = [10, 20, 15, 25, 22] -> my_result [2, 4, 15, 25]
 
-# my_list_1 = [1, 5, 3, 2, 4, 4, 7, 8, 10, 100]
-# my_list_2 = [2, 7, 5, 2, 2, 6, 3, 9, 7, 7, 10]
-# my_result = []
-#
-# for value in my_list_1:
-#     if not value % 2:
-#         my_result.append(value)
-#
-# for value in my_list_2:
-#     if value % 2:
-#         my_result.append(value)
-#
-# print(my_result)
+my_list_1 = [1, 5, 3, 2, 4, 4, 7, 8, 10, 100]
+my_list_2 = [2, 7, 5, 2, 2, 6, 3, 9, 7, 7, 10]
+my_result = []
+
+for value in my_list_1:
+    if not value % 2:
+        my_result.append(value)
+
+for value in my_list_2:
+    if value % 2:
+        my_result.append(value)
+
+print(my_result)
 
 ###
 
 # 4. Дан список my_list. СОЗДАТЬ НОВЫЙ список new_list у которого первый элемент из my_list
 # стоит на последнем месте. Если my_list [1,2,3,4], то new_list [2,3,4,1]
 
-# my_list = [1, 2, 3, 4, 5]   # 2, 3, 4, 5, 1
-# new_list = []
+my_list = [1, 2, 3, 4, 5]   # 2, 3, 4, 5, 1
+new_list = []
 
 # 1)
 
@@ -109,30 +109,21 @@
 
 # 2) удовлетворяет условию намного больше чем первый
 
-# new_list = my_list[1:].copy()
-# new_list.append(my_list[0])
-# print(new_list)
+new_list = my_list[1:].copy()
+new_list.append(my_list[0])
+print(new_list)
 
 ###
 
 # 5.Дан список my_list. В ЭТОМ списке первый элемент переставить на последнее место.
 # [1,2,3,4] -> [2,3,4,1]. Пересоздавать список нельзя! (используйте метод pop)
 
-# my_list = [1, 2, 3, 4, 5]   # 2, 3, 4, 5, 1
+my_list = [1, 2, 3, 4, 5]
 
-# 1)
+my_list.append(my_list[0])
+my_list.pop(0)
 
-# my_list.append(my_list[0])
-# my_list.pop(0)
-#
-# print(my_list)
-
-# 2)
-
-# tmp = my_list[0]
-# my_list.pop(0)
-# my_list.append(tmp)
-# print(my_list)
+print(my_list)
 
 ###
 
@@ -140,17 +131,17 @@
 # Например "43 больше чем 34 но меньше чем 56". Найти сумму ВСЕХ ЧИСЕЛ (А НЕ ЦИФР) в этой строке.
 # Для данного примера ответ - 133.
 
-# my_string = "43 больше чем 34 но меньше чем 56"
-# my_string = my_string.split(' ')
-#
-# result = 0
-#
-# for value in my_string:
-#     if value.isdigit():
-#         value = int(value)
-#         result += value
-#
-# print(result)
+my_string = "43 больше чем 34 но меньше чем 56"
+my_string = my_string.split(' ')
+
+result = 0
+
+for value in my_string:
+    if value.isdigit():
+        value = int(value)
+        result += value
+
+print(result)
 
 ###
 
@@ -158,19 +149,18 @@
 # Если строка содержит нечетное количество символов, пропущенный второй символ последней пары должен
 # быть заменен подчеркиванием ('_'). Примеры: 'abcd' -> ['ab', 'cd'], 'abcde' -> ['ab', 'cd', e_']
 
-# my_str = 'abcd'
-# if not len(my_str) % 2:
-#     my_str = my_str[:2] + ' ' + my_str[2:]
-#     my_str = my_str.split()
-#     print(my_str, type(my_str))
-# else:
-#     my_str = my_str[:2] + ' ' + my_str[2:]
-#     tmp = my_str[-1]
-#     my_str = my_str.replace(my_str[-1], '')
-#     my_str = my_str + ' ' + tmp + '_'
-#     my_str = my_str.split()
-#     print(my_str)
-# # знаю, что решил по хитрому, но я не знаю как по-другому
+my_str = str(input("Enter your string:"))
+result = []
+tmp = int(len(my_str) / 2)
+
+if not len(my_str) % 2:
+    for index in range(tmp):
+        result += [my_str[index * 2: index * 2 + 2]]
+else:
+    for index in range(tmp + 1):
+        result += [my_str[index * 2: index * 2 + 2]]
+    result[-1] += '_'
+print(result)
 
 ###
 
@@ -178,6 +168,9 @@
 # которые точно находятся в этой строке. Причем l_limit левее чем r_limit.
 # В переменную sub_str поместить часть строки между этими символами.
 # my_str = "My_long str", l_limit = "o", r_limit = "t" -> sub_str = "ng s"
+# решить через find, rfind
+
+# 1)
 
 # my_str = "My_long str"
 # l_limit = "o"
@@ -201,4 +194,38 @@
 # sub_str = my_str[count_l + 1:count_r]
 # print(sub_str)
 
+# 2) более легкий
+
+my_str = "My_long str"
+l_limit = "o"
+r_limit = "t"
+
+print(my_str[my_str.find(l_limit) + 1:my_str.find(r_limit)])
+
 ###
+
+# 9. Дана строка my_str в которой символы МОГУТ повторяться и два символа l_limit, r_limit,
+# которые точно находятся в этой строке. Причем l_limit левее чем r_limit.
+# В переменную sub_str поместить НАИБОЛЬШУЮ часть строки между этими символами.
+# my_str = "My long string", l_limit = "o", r_limit = "g" -> sub_str = "ng strin".
+
+my_str = "My long string"
+l_limit = "o"
+r_limit = "g"
+
+print(my_str[my_str.find(l_limit) + 1:my_str.rfind(r_limit)])
+
+###
+
+# 10. Дан список чисел. Определите, сколько в этом списке элементов,
+# которые больше суммы двух своих соседей (слева и справа), и НАПЕЧАТАЙТЕ КОЛИЧЕСТВО таких элементов.
+# Крайние элементы списка никогда не учитываются, поскольку у них недостаточно соседей.
+# Для списка [2,4,1,5,3,9,0,7] ответом будет 3 потому что 4 > 2+1, 5 > 1+3, 9>3+0.
+
+my_list = [2, 4, 1, 5, 3, 9, 0, 7]
+count = 0
+
+for index in range(1, len(my_list) - 1):
+    if my_list[index - 1] + my_list[index + 1] < my_list[index]:
+        count += 1
+print(count)
